9486bb4d66045e844dc13dcebd932fb75726233b
YasmineB25 <99327675+YasmineB25@users.noreply.github.com>
2024-01-19 13:40:48 +0100

    Update2

--- src/main/java/yasmine/exam/demo/TacheInexistanteException.java
+++ src/main/java/yasmine/exam/demo/TacheInexistanteException.java
@@ -3,9 +3,9 @@ package yasmine.exam.demo;
 
 public class TacheInexistanteException extends Exception {
     public TacheInexistanteException(String message) {
-       // super(message);
+        super(message);
 
-       System.out.println("La tâche avec le titre '" + message + "' n'existe pas.");
+      // System.out.println("La tâche avec le titre '" + message + "' n'existe pas.");
     }
 }
 
--- src/test/java/yasmine/exam/demo/GestionTachesTest.java
+++ src/test/java/yasmine/exam/demo/GestionTachesTest.java
@@ -3,7 +3,6 @@ package yasmine.exam.demo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
